a4241057024893943c26f0d63afeb02f395b2e81
slightlycyborg <taggartmachines@gmail.com>
2015-06-09 09:14:34 -0400

    seperated robot from world

--- world.py
+++ world.py
@@ -1,12 +1,10 @@
 import os, sys, math, queue
-from robot import Robot
 from PIL import Image
 import pygame_sdl2
 pygame_sdl2.import_as_pygame()
 import pygame
 from pygame.locals import *
 from robot import Robot
-from gardenbotnn import Garden_Bot_NN
 
 pygame.font.init()
 
@@ -22,16 +20,6 @@ SPRT_RECT_Y=0
 LEN_SPRT_X=100
 LEN_SPRT_Y=100
 #This is the length of the sprite
-
-def load_pygame_img(file_name):
-    """Load img is a function that uses PIL to load a pygame image"""
-    img = Image.open(file_name)
-    mode = img.mode
-    size = img.size
-    data = img.tostring()
-    #pygame_img = pygame.image.fromstring(data, size, mode) #Load the sheet
-    pygame_img = pygame.image.load(file_name)
-    return pygame_img
 
 
 
